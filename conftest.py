fdc17e2a62b8f9df4b61d39a152f3e0125a8ee1e
Jordan Kim Jubilo <jordan.kim.jubilo@gmail.com>
2026-02-28 09:38:50 -0800

    update workflow

--- conftest.py
+++ conftest.py
@@ -1,12 +1,33 @@
+import os
+
 import pytest
 from playwright.sync_api import sync_playwright
 
 
 @pytest.fixture
-def playwright_page():
+def playwright_page(request):
+    test_name = request.node.name
+    root_dir = request.config.rootpath
+
+    video_path = os.path.join(root_dir, "tests-results","videos",str(test_name))
+    screenshot_path = os.path.join(root_dir, "tests-results", "screenshots", str(test_name))
+
+    #Create directory only if not present
+    if not os.path.exists(video_path):
+        os.makedirs(video_path, exist_ok=True)
+
     with sync_playwright() as p:
-        browser = p.chromium.launch()
-        context = browser.new_context()
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(record_video_dir=video_path)
         page = context.new_page()
-        yield page
-        browser.close()
+
+        try:
+            yield page
+        finally:
+            page.screenshot(path=f"{screenshot_path}/{test_name}.png")
+
+            # VERY IMPORTANT: close context to finalize video
+            context.close()
+
+            # Now it's safe to close browser
+            browser.close()
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,11 @@
-from playwright.sync_api import sync_playwright
+import pytest
+from playwright.sync_api import sync_playwright, expect
 from src.pages.login_page import LoginPage
 
 
 class TestExamples:
+
+    @pytest.mark.page_title
     def test_page_title(self):
         with sync_playwright() as p:
             browser = p.chromium.launch()
@@ -10,13 +13,14 @@ class TestExamples:
             page.goto("https://blisscoders.pythonanywhere.com/")
 
             assert "Playwright" in page.title()
-
             browser.close()
 
+    @pytest.mark.valid_credentials
     def test_valid_login(self, playwright_page):
         login_page = LoginPage.open(page=playwright_page)
         login_page.login(username="admin@test.com",password="Password123")
 
-    # def test_invalid_login(self, playwright_page):
-    #     login_page = LoginPage.open(playwright_page)
-    #     login_page.login(username="test",password="123445")
+    @pytest.mark.invalid_credentials
+    def test_invalid_login(self, playwright_page):
+        login_page = LoginPage.open(playwright_page)
+        login_page.login(username="test",password="123445")
